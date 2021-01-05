d225d1515b6cfd88f9c65f6d89927cc596fadc39
KonradG13 <graczykowski.k@gmail.com>
2021-01-05 20:52:55 +0100

    only alternate colouring left!

--- script.js
+++ script.js
@@ -1,10 +1,64 @@
 const container = document.querySelector('.container');
-const width = 16;
-const length = 16;
-const elements = length*width;
+var width = 16;
+var length = 16;
 
-for (i = 0; i<elements; i++ ){
-    const gridbox = document.createElement('div');
+
+
+var elements = length*width;
+for (i = 0; i<elements; i++ ) {
+    var gridbox = document.createElement('div');
 gridbox.classList.add('box');
 container.appendChild(gridbox);
+};
+
+var boxes = document.querySelectorAll('.box')
+
+boxes.forEach( item => { item.addEventListener('mouseenter', () => 
+{item.setAttribute('style', 'background-color: black;')})
+     
+ });
+
+
+ const resetbutton = document.createElement('button')
+ resetbutton.innerHTML = "Redraw the grid!";
+ resetbutton.setAttribute('style', 'margin-bottom: 25px;')
+ resetbutton.setAttribute('id', 'resetbutton');
+ document.body.prepend(resetbutton);
+
+var redrawGrid = function () {
+
+    var customWidth = prompt("How many columns?", 16);
+    var customLength = prompt("How many rows?", 16);
+    var customElements = customLength*customWidth
+    
+    var oldBoxes = document.querySelector('.container');
+    while (oldBoxes.firstChild) {oldBoxes.removeChild(oldBoxes.lastChild)};
+    
+    var pxWidth = Math.min(window.innerWidth/customWidth*0.99, 20);
+    var pxLength = Math.min((window.innerHeight-50)/customLength*0.99, 20);
+    var pxSize = Math.min(pxWidth, pxLength);
+
+    var gridStyle = document.querySelector('.container');
+    gridStyle.style.setProperty('grid-template-columns', 'repeat(' + customWidth + ', '+ pxSize + 'px');
+    gridStyle.style.setProperty('grid-template-rows', 'repeat(' + customLength + ', '+ pxSize + 'px');
+    
+    
+    for (i = 0; i<customElements; i++ ) {
+        var gridbox = document.createElement('div');
+    gridbox.classList.add('box');
+    container.appendChild(gridbox);
+    };
+
+    const boxStyle = document.querySelectorAll('.box');
+     
+    boxStyle.forEach( item => { item.addEventListener('mouseenter', () => 
+    {item.setAttribute('style', 'background-color: black;')})
+         
+     });
 }
+
+
+
+ const thebutton = document.querySelector('#resetbutton')
+ thebutton.addEventListener('click', redrawGrid);
+ 
