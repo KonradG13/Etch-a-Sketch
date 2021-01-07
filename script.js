ee7db0ffd43d79011ded533ccc48d29dae9266e2
KonradG13 <69989729+KonradG13@users.noreply.github.com>
2021-01-07 11:04:46 +0100

    Finished project!
    uploading here - git crash

--- script.js
+++ script.js
@@ -1,41 +1,91 @@
-const container = document.querySelector('.container');
-var width = 16;
-var length = 16;
+//functions
+var randomRGBgenerator = function() { return Math.floor(Math.random()*256) }; //generates random number from 1 to 255
 
+function colorswitch(i)  {                                                    //switch eventlisteners based on selectionbox
+    
+    var clone = container.cloneNode(true);
+    document.body.replaceChild(clone, container)
+    container = clone
 
-
-var elements = length*width;
-for (i = 0; i<elements; i++ ) {
-    var gridbox = document.createElement('div');
-gridbox.classList.add('box');
-container.appendChild(gridbox);
+    var boxes = document.querySelectorAll('.box')
+    
+    switch(i) {
+        case 0:                
+            boxes.forEach( item => { item.addEventListener('mouseenter', () => whiteToBlack(item))  
+         });
+            break;
+        case 1:    
+            boxes.forEach( item => { item.addEventListener('mouseenter', () => randomColor(item));
+            });
+            break;
+        case 2:                  
+            boxes.forEach( item => { item.addEventListener('mouseenter', () => gradualDarknessWB(item)
+            )});
+            break;
+        case 3:
+            boxes.forEach( item => { item.addEventListener('mouseenter', () => 
+                gradualDarkeningFirst(item),
+                 {once: true} );                 
+            });
+            boxes.forEach( item => { item.addEventListener('mouseenter', () => gradualDarkeningColor(item));                 
+            });
+            break;
+    };
+    colorBox.addEventListener('change', ()=> {
+        var colorOption = colorBox.selectedIndex;
+        colorswitch(colorOption)});
+    thebutton.addEventListener('click', redrawGrid);     
 };
 
-var boxes = document.querySelectorAll('.box')
+function whiteToBlack(item) {if (colorBox.selectedIndex == 0) {item.setAttribute('style', 'background-color: black;')}}; //change square color to black
 
-boxes.forEach( item => { item.addEventListener('mouseenter', () => 
-{item.setAttribute('style', 'background-color: black;')})
-     
- });
+function randomColor(item) {if (colorBox.selectedIndex == 1) {                          //change square color to random
+    item.setAttribute('style', 'background-color: rgb('+randomRGBgenerator()+
+    ', '+randomRGBgenerator()+', '+randomRGBgenerator()+')')}};
 
+function gradualDarknessWB(item) {if (colorBox.selectedIndex == 2) {                    // change square color to black but with increasing opacity, starting from 0.1
+    var itemOpacity = parseFloat(getComputedStyle(item).getPropertyValue("opacity"))
+    
+    if (item.style.opacity == '' || item.style.backgroundColor !== "black" || item.getAttribute('style').includes('opacity')===false ) 
+     {
+      item.style.opacity = '0.1';
+      item.style.backgroundColor = 'black';}
+    else { 
+        itemOpacity = Math.min(itemOpacity + 0.1, 1.0)
+        item.style.opacity = itemOpacity.toString()}        
+}};
 
- const resetbutton = document.createElement('button')
- resetbutton.innerHTML = "Redraw the grid!";
- resetbutton.setAttribute('style', 'margin-bottom: 25px;')
- resetbutton.setAttribute('id', 'resetbutton');
- document.body.prepend(resetbutton);
+function gradualDarkeningFirst(item) {if (colorBox.selectedIndex == 3) {                // change background color to random RGB color
+    item.setAttribute('style', 'background-color: rgb('+randomRGBgenerator()+
+    ', '+randomRGBgenerator()+', '+randomRGBgenerator()+')');
+    item.style.opacity = '0.0';
+}}
+
+function gradualDarkeningColor(item) {if (colorBox.selectedIndex == 3) {
+    item.style.opacity = Math.min(parseFloat(item.style.opacity)+0.1, 1.0).toString();  //increase opacity regardless of color
+}}
 
 var redrawGrid = function () {
 
-    var customWidth = prompt("How many columns?", 16);
-    var customLength = prompt("How many rows?", 16);
+    var customWidth = prompt("How many columns (max.100)?", 16);
+    
+    while (isNaN(customWidth*1)) {
+        customWidth = prompt("Type a number! How many columns (max.100)?", 16);
+    };
+    
+    var customLength = prompt("How many rows (max.100)?", 16);
+    
+    while (isNaN(customLength*1)) {
+        customLength = prompt("Type a number! How many rows (max.100)?", 16);
+    };
+
     var customElements = customLength*customWidth
     
     var oldBoxes = document.querySelector('.container');
     while (oldBoxes.firstChild) {oldBoxes.removeChild(oldBoxes.lastChild)};
     
-    var pxWidth = Math.min(window.innerWidth/customWidth*0.99, 20);
-    var pxLength = Math.min((window.innerHeight-50)/customLength*0.99, 20);
+    var pxWidth = Math.max(640/customWidth, 6.4);
+    var pxLength = Math.max(640/customLength, 6.4);
     var pxSize = Math.min(pxWidth, pxLength);
 
     var gridStyle = document.querySelector('.container');
@@ -49,16 +99,67 @@ var redrawGrid = function () {
     container.appendChild(gridbox);
     };
 
-    const boxStyle = document.querySelectorAll('.box');
-     
-    boxStyle.forEach( item => { item.addEventListener('mouseenter', () => 
-    {item.setAttribute('style', 'background-color: black;')})
-         
-     });
-}
+    var colorOption = colorBox.selectedIndex;
+    colorswitch(colorOption);       
+   
+}; 
+
+//initial grid drawing
+var container = document.querySelector('.container');
+var width = 16;
+var length = 16;
 
 
+var elements = length*width;
+for (i = 0; i<elements; i++ ) {
+    var gridbox = document.createElement('div');
+    gridbox.classList.add('box');
+    container.appendChild(gridbox);
+};
+var boxes = document.querySelectorAll('.box')
+boxes.forEach( item => { item.addEventListener('mouseenter', () => whiteToBlack(item))});
 
- const thebutton = document.querySelector('#resetbutton')
- thebutton.addEventListener('click', redrawGrid);
+//Redraw the grid button
+const resetbutton = document.createElement('button')
+resetbutton.innerHTML = "Redraw the grid!";
+resetbutton.setAttribute('style', 'margin-bottom: 25px;')
+resetbutton.setAttribute('id', 'resetbutton');
+document.body.prepend(resetbutton);
+
+const thebutton = document.querySelector('#resetbutton')
+thebutton.addEventListener('click', redrawGrid);
+
+//Coloring type selector
+const coloringSelection = document.createElement('select')
+coloringSelection.setAttribute('id', 'selectionbox')
+coloringSelection.style.marginBottom = '80px'
+coloringSelection.style.marginLeft = '20px'
+
+const firstOption = document.createElement('option')
+firstOption.setAttribute('value', '1')
+firstOption.innerHTML = "All To Black!"
+coloringSelection.appendChild(firstOption);
+
+const secondOption = document.createElement('option')
+secondOption.setAttribute('value', '2')
+secondOption.innerHTML = "Random Colors!"
+coloringSelection.appendChild(secondOption);
+
+const thirdOption = document.createElement('option')
+thirdOption.setAttribute('value', '3')
+thirdOption.innerHTML = "Gradually Darkening!"
+coloringSelection.appendChild(thirdOption);
+
+const fourthOption = document.createElement('option')
+fourthOption.setAttribute('value', '4')
+fourthOption.innerHTML = "Gradually Darkening Random Colors!"
+coloringSelection.appendChild(fourthOption);      
+
+document.body.insertBefore(coloringSelection, document.body.childNodes[1]);
  
+const colorBox = document.querySelector('#selectionbox');
+colorBox.addEventListener('change', ()=> {
+    var colorOption = colorBox.selectedIndex;
+    colorswitch(colorOption)      
+});
+
